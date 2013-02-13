732540dbe0dd72cc174f72c29287f2186e424751
Siddhartha Jana <sidjana@coil.cs.uh.edu>
2013-02-13 03:37:03 -0600

    Adding results of validation for the validation. Also, making the npb suite compiler agnostic

--- performance/npb/test_npb.sh
+++ performance/npb/test_npb.sh
@@ -2,49 +2,58 @@
 
 source ../support/CONFIG
 
-if [ $# == 1 ]; then
+if [ $# == 2 ]; then
   if [ "$1" == "compile" ]; then
     COMPILE_TESTS="1"
+    compiler=$2
   elif [ "$1" == "execute" ]; then
     EXECUTE_TESTS="1"
+    compiler=$2
   elif [ "$1" == "complete" ]; then
     BOTH="1"
+    compiler=$2
   else
     echo "USAGE:  compile | execute | complete"
   fi
 else
-  echo "USAGE: compile | execute | complete"
+  echo "USAGE: test_kernels.sh [mode] [compiler] where "
+  echo "           mode     = compile|execute|complete"
+  echo "           compiler = uhcaf|ifort|g95"
+  compiler=uhcaf
+  BOTH="1"
+  echo -e "Using defaults:  test_kernels.sh complete uhcaf \n\n"
 fi
 
 # delete past regression results and make folders if needed
 rm -rf $COMP_OUT_DIR $EXEC_OUT_DIR $BIN_DIR
 mkdir -p $COMP_OUT_DIR $EXEC_OUT_DIR  $HISTORY_OUT_DIR $BIN_DIR $LOG_DIR
 
-printf '%8s %8s %8s %15s %15s %10s \n' "<NAME>" "<CLASS>" "<NPROCS>" "<COMPILATION>" "<EXECUTION>" "<RESULT>" 
+printf '%8s %8s %8s %15s %15s %10s %15s \n' "<NAME>" "<CLASS>" "<NPROCS>" "<COMPILATION>" "<EXECUTION>" "<RESULT>" "<TIME(secs)>" | tee -a $LOG_DIR/$logfile  
 
-
-for BM in ep sp bt 
+cp ./config/make.def.$compiler ./config/make.def
+for BM in ep sp bt cg 
 do
 	for CLASS in S A B C
 	do
   		  if [ "$BM" == "ep" -o "$BM" == "cg" ]; then
-  		       NPROCS="1 2 4 8 16";
+  		       NPROCS_LST="1 2 4 8 16";
   		  else
-  		       NPROCS="1 4 9 16"    
+  		       NPROCS_LST="1 4 9 16"    
   		  fi
 
-  		  for NP in  $NPROCS
+  		  for NP in  $NPROCS_LST
   		  do
+		  	NPROCS=$NP
+			source ${BENCH_PATH}/../support/CONFIG-compiler.$compiler
+			cp ./config/make.def.$compiler ./config/make.def
   		     	opfile=$BM.$CLASS.$NP
 			printf '%6s %6s %6s ' "$BM" "$NP" "$CLASS" 
   		     	if [ "$COMPILE_TESTS"=="1" -o "$BOTH"=="1" ]; then
 			
 			 	# adding make.def support for intel
-			 	sed "s/-coarray-num-images=.* /-coarray-num-images=$NPROCS /g" ./config/make.def > ./config/make.def.intel
-			 	cp ./config/make.def.intel ./config/make.def
-
+			 	#sed "s/-coarray-num-images=.* /-coarray-num-images=$NP /g" ./config/make.def > ./config/make.def.intel
 			 	make clean &>/dev/null
-  		     	 	COMPILE_OUT=`make $BM NPROCS=$NP CLASS=$CLASS >>$COMP_OUT_DIR/$opfile.compile 2>&1 && echo 1 || echo -1`
+  		     	 	COMPILE_OUT=`make $BM NPROCS=$NP CLASS=$CLASS COMPILER=$compiler >>$COMP_OUT_DIR/$opfile.compile 2>&1 && echo 1 || echo -1`
   		     	 	if [ "$COMPILE_OUT" == "1" ]; then
   		     	 		 COMPILE_STATUS="PASS"
   		     	 	else
@@ -54,9 +63,10 @@ do
 			printf '%15s ' "$COMPILE_STATUS"
   		     	if [ "$EXECUTE_TESTS" -eq "1" -o "$BOTH" -eq "1" ]; then           #execution enabled
 			      VERIFICATION="UNKNOWN"
+			      TIME="--"
   		     	      if [ -f  $BIN_DIR/$opfile ]; then  #compilation passed
 
-  		     	         EXEC_OUT=` perl $ROOT/../../support/timedexec.pl $TIMEOUT "$EXEC_CMD -np $NP $BIN_DIR/$opfile " &> $EXEC_OUT_DIR/$opfile.exec && echo 1||echo -1`
+  		     	         EXEC_OUT=` perl $ROOT/../../support/timedexec.pl $TIMEOUT "$LAUNCHER $BIN_DIR/$opfile $EXEC_OPTIONS " &> $EXEC_OUT_DIR/$opfile.exec && echo 1||echo -1`
   		     	         if [ "$EXEC_OUT" == "-1" ]; then                         #runtime error
   		     	             EXEC_STATUS="RUNTIME ERROR"
 				     FAILED_COUNT=$(($FAILED_COUNT+1))
@@ -64,12 +74,13 @@ do
 				     EXEC_STATUS="PASS"
 				     PASSED_COUNT=$(($PASSED_COUNT+1))
 				     VERIFICATION=`grep -oh "\w*SUCCESSFUL"  $EXEC_OUT_DIR/$opfile.exec`
+				     TIME=`grep "seconds"  $EXEC_OUT_DIR/$opfile.exec | sed 's/.*=//g' |sed 's/\s\+//g' `
   		     	         fi
   		     	     else
   		     	         EXEC_STATUS="NO BINARY"                                   #compilation failed
   		     	     fi
   		     	fi
-  		     	printf '%15s %18s\n' "$EXEC_STATUS" "$VERIFICATION" | tee -a $LOG_DIR/$logfile 
+  		     	printf '%15s %18s %15s\n' "$EXEC_STATUS" "$VERIFICATION" "$TIME" | tee -a $LOG_DIR/$logfile 
   		  done
 	done
 done
@@ -81,3 +92,4 @@ echo "Results of this performance run can be found in: $LOG_DIR/$logfile"
 # backing up results to HISTORY folder
 cp -r  $COMP_OUT_DIR  $HISTORY_OUT_DIR/compile_$DATE
 cp -r  $EXEC_OUT_DIR  $HISTORY_OUT_DIR/execute_$DATE
+rm -rf ./config/make.def
