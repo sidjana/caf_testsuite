940b69c99508ac5d78a48409bd9307132d51291a
Siddhartha Jana <siddharthajana24@gmail.com>
2013-10-05 02:56:30 -0500

    Replacing all shell scripts with Makefiles Modification of kernel tests with the FORTRAN timers Display of timing information of execution of kernels

---
+++ performance/kernels/src/mm/mm.f90
@@ -0,0 +1,62 @@
+      program matrixmul
+        implicit none
+        integer,parameter :: N = 500
+        integer :: P
+        real,allocatable,dimension(:,:),codimension[:,:] :: a,b,c
+        integer :: i,j,k,l,q,iAm, np
+        integer :: myP, myQ
+        integer :: ticks, start_time, end_time, rate
+
+        np = num_images()
+        iAm = this_image()
+
+        ! check that np is a square
+        P = INT(sqrt(REAL(np)))
+        if ( P*P /= np ) then
+          if (iAm == 1)   &
+            write(*,"('num_images must be square: p=',i5)") np
+          stop
+        end if
+
+
+        if (P*P /= np)  then
+        end if
+
+        allocate(a(N,N)[P,*], b(N,N)[P,*], c(N,N)[P,*])
+
+        myP = this_image(a,1)
+        myQ = this_image(a,2)
+
+        a = 1.0
+        b = 1.0
+        c = 0.0
+
+        sync all
+
+        call system_clock(start_time, rate)
+
+          do i=1,N
+            do j=1,N
+              do l=1,P
+                c(i,j) = c(i,j) + sum(a(i,:)[myP,l]*b(:,j)[l,myQ])
+              end do
+            end do
+          end do
+
+          sync all
+          call system_clock(end_time)
+
+
+          if (any(c /= N*P)) write(*,"('error on image: ',2i5,e20.10)") &
+                 myP,myQ,c(1,1)
+          write(*,"('check sum[',i5',',i5,']',e20.10)") &
+                  myP,myQ, sum(c) - P*N**3
+
+    if (iAm == 1) then
+        ticks = end_time - start_time
+        write(*, '(//A20,I8,A)')   "clock rate = ", rate, " ticks/s"
+        write(*, '(A20,I8)')           "ticks  = ", ticks
+        write(*, '(A20,F8.2,A)') "elapsed time = ", ticks/(1.0*rate), " seconds"
+    end if
+
+      end program matrixmul
--- performance/microbenchmarks/test_microbenchmarks.sh
+++ performance/microbenchmarks/test_microbenchmarks.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ -f ../support/CONFIG ]; then
   source ../support/CONFIG
@@ -8,71 +8,87 @@ fi
 
 if [ "$1" == "cleanall" ]; then
     rm -rf $LOG_DIR $BIN_DIR
+    rm -rf $TESTS_DIR/*.mod
     exit 0
 fi
+cd $TESTS_DIR ; FILE_LIST="`ls *.f90`" ; cd -
 
-if [ $# == 2 ]; then
+if [ $# == 3 ]; then
   if [ "$1" == "compile" ]; then
     COMPILE_TESTS="1"
+    EXECUTE_TESTS="0"
     compiler=$2
   elif [ "$1" == "execute" ]; then
+    COMPILE_TESTS="0"
     EXECUTE_TESTS="1"
     compiler=$2
   elif [ "$1" == "complete" ]; then
     BOTH="1"
     compiler=$2
   else
-  echo "USAGE: test_microbenchmarks.sh [mode [compiler] ] where "
-  echo "           mode     = compile|execute|complete"
-  echo "           compiler = uhcaf|ifort|g95"
-  echo "The results of all the microbenchmarks are stored in a plottable format in $EXEC_OUT_DIR"
+    echo "USAGE: ./test_microbenchmarks.sh [mode [compiler] ] where "
+    echo "           mode     = compile|execute|complete"
+    echo "           compiler = uhcaf|ifort|g95"
+    echo "The results of all the microbenchmarks are stored in a plottable format in $EXEC_OUT_DIR"
     exit 1
   fi
+  if [ "$3" != "" ]; then
+  	if [ -f $TESTS_DIR/$3.f90 ]; then
+		FILE_LIST="$3.f90"
+		echo "file found"
+	else
+	        echo "File  $3.f90 not found !"
+		exit 1
+	fi
+  fi
 else
-  echo "USAGE: test_microbenchmarks.sh [mode] [compiler] where "
-  echo "           mode     = compile|execute|complete"
-  echo "           compiler = uhcaf|ifort|g95"
-  echo -e "Please ensure:\n The test_suite specific parameters are set in ${BENCH_PATH}/../support/CONFIG \n The compiler specific parameters in ${BENCH_PATH}/../support/CONFIG-compiler.<compiler> \n"
-  exit 1
+    echo "USAGE: ./test_microbenchmarks.sh [mode] [compiler] where "
+    echo "           mode     = compile|execute|complete"
+    echo "           compiler = uhcaf|ifort|g95"
+    echo -e "Please ensure:\n The test_suite specific parameters are set in ${BENCH_PATH}/../support/CONFIG \n The compiler specific parameters in ${BENCH_PATH}/../support/CONFIG-compiler.<compiler> \n"
+    exit 1
 fi
 
-if [ -f ../support/CONFIG-compiler.${compiler} ]; then
-continue
-else
+if [ ! -f ../support/CONFIG-compiler.${compiler} ]; then
   echo "CONFIG-compiler.${compiler} file missing. Please ensure that CONFIG file is present under $ROOT/../support"
   exit 1
 fi
 
-# delete past regression results and make folders if needed
-rm -rf $COMP_OUT_DIR $EXEC_OUT_DIR
+# delete past results and make folders if needed
+rm -rf $COMP_OUT_DIR $EXEC_OUT_DIR 
 mkdir -p $COMP_OUT_DIR $EXEC_OUT_DIR  $HISTORY_OUT_DIR $BIN_DIR $LOG_DIR
 
 cd $TESTS_DIR
-
 $CC -c rtc.c -o $BIN_DIR/rtc.o -D$TIMER_ARCH
 
-printf '%20s %5s %20s %20s\n' "<NAME>" "<NPROCS>" "<COMPILATION>" "<EXECUTION>"  | tee -a $LOG_DIR/$logfile
-
-
-for file in `ls *.f90`; do
+if [ "$COMPILE_TESTS" -eq "1" -o "$BOTH" -eq "1" ]; then
+ for file in $FILE_LIST ; do
        NP=2
        NPROCS=$NP
        source ${BENCH_PATH}/../support/CONFIG-compiler.${compiler}
        type=`echo $file | awk -F"/" '{print $NF}'`
-       opfile=$type.$NP
+       opfile=$type.out
        logfile=$DATE.log
-       printf '%20s %5s ' "$type" "$NP"  | tee -a $LOG_DIR/$logfile
-       if [ "$COMPILE_TESTS" == "1" -o "$BOTH"=="1" ]; then
         COMPILE_OUT=`$COMPILE_CMD  $type $BIN_DIR/rtc.o -o $BIN_DIR/$opfile >>$COMP_OUT_DIR/$opfile.compile 2>&1 && echo 1 || echo -1`
         if [ "$COMPILE_OUT" -eq "1" ]; then
           COMPILE_STATUS="PASS"
         else
           COMPILE_STATUS="FAIL"
         fi
-       fi
-       printf '%20s ' "$COMPILE_STATUS"  | tee -a $LOG_DIR/$logfile
-       if [ "$EXECUTE_TESTS" -eq "1" -o "$BOTH" -eq "1" ]; then           #execution enabled
-             if [ -f  $BIN_DIR/$opfile ]; then  #compilation passed
+       printf 'Test: %s\tCompilation: %s\n' "$type" "$COMPILE_STATUS" | tee -a $LOG_DIR/$logfile
+ done
+fi
+
+if [ "$EXECUTE_TESTS" -eq "1" -o "$BOTH" -eq "1" ]; then           #execution enabled
+ for file in $FILE_LIST ; do
+       NP=2
+       NPROCS=$NP
+       source ${BENCH_PATH}/../support/CONFIG-compiler.${compiler}
+       type=`echo $file | awk -F"/" '{print $NF}'`
+       opfile=$type.out
+       logfile=$DATE.log
+
+       if [ -f  $BIN_DIR/$opfile ]; then  #compilation passed
                 EXEC_OUT=` perl $ROOT/../../support/timedexec.pl $TIMEOUT "$LAUNCHER $BIN_DIR/$opfile $EXEC_OPTIONS  "  &> $EXEC_OUT_DIR/$opfile.exec  && echo 1||echo -1`
 
                 if [ "$EXEC_OUT" == "-1" ]; then                         #runtime error
@@ -84,15 +100,16 @@ for file in `ls *.f90`; do
                 fi
             else
                 EXEC_STATUS="NO BINARY"                                   #compilation passed
-            fi
        fi
-       printf '%20s\n' "$EXEC_STATUS" | tee -a $LOG_DIR/$logfile
-done
-
-echo "______________________________EXECUTION STATISTICS (not compilation)__________________________" | tee -a $LOG_DIR/$logfile
+       echo "--------------------------------------"
+       printf 'Test: %s\tNumber of Images: %s\tExecution Status: %s\n' "$type" "$NP" "$EXEC_STATUS" | tee -a $LOG_DIR/$logfile
+       cat $EXEC_OUT_DIR/$opfile.exec  2>/dev/null
+ done
+echo "______________________________EXECUTION STATISTICS__________________________" | tee -a $LOG_DIR/$logfile
 echo "TOTAL PASSED = $PASSED_COUNT TOTAL FAILED = $FAILED_COUNT"  | tee -a $LOG_DIR/$logfile
 echo "Results of this performance run can be found in: $LOG_DIR/$logfile"
 echo "The results of all the microbenchmarks are stored in plottable format in $EXEC_OUT_DIR"
+fi
 
 # backing up results to HISTORY folder
 cp -r  $COMP_OUT_DIR  $HISTORY_OUT_DIR/compile_$DATE
